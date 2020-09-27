b0baf0a53e1c7bb358ef0409fe41102a08e6020c
cesar-pimenta <cesar-pimenta@live.com>
2020-09-27 01:07:57 -0300

    configure static

--- web_site/urls.py
+++ web_site/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index_blog, post_detail
 
 urlpatterns = [
     path('', index_blog, name='init_blog'),
     path('post/<int:id>/', post_detail, name='post_detail'),
-]
+] 
