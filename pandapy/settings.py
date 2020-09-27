0183c955da0579d6315acae87aab16b75de1c9ad
cesar-pimenta <cesar-pimenta@live.com>
2020-09-26 21:21:16 -0300

    correção

--- pandapy/settings.py
+++ pandapy/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '34.68.122.29']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '34.68.122.29']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
