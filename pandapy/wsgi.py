339278f745e1c530e8feecef041bbf64c6c415a7
cesar-pimenta <cesar-pimenta@live.com>
2020-09-26 21:03:08 -0300

    simple app blogw

--- pandapy/wsgi.py
+++ pandapy/wsgi.py
@@ -1,6 +1,9 @@
-import os
+import os, sys 
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/cesarpimentajs/pandapy')
+sys.path.append('/home/cesarpimentajs/venv/Lib/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandapy.settings')
 
